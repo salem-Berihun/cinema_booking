0b5632af8c55102f614924dada59b8aabf4b7ac2
salem-Berihun <salemberihun6@gmail.com>
2025-08-05 02:21:53 -0900

    Update ReportGenerator.java

--- src/interfaces/ReportGenerator.java
+++ src/interfaces/ReportGenerator.java
@@ -3,7 +3,7 @@ import model.Booking;
 public interface ReportGenerator {
 
     /**
-     * Generates a generic report based on the report type and provided data.
+     * Generates a generic report based on the report type and provided data
      * The implementation will determine how to interpret 'reportType' and 'data'.
      *
      * @param reportType A string indicating the type of report to generate (e.g., "DAILY_SALES", "MOVIE_PERFORMANCE").
