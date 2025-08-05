1f248c934906f11be4c21264165f8b0b82ae617c
salem-Berihun <salemberihun6@gmail.com>
2025-08-04 20:42:01 -0900

    Update Movie.java

--- model/Movie.java
+++ model/Movie.java
@@ -1,30 +1,78 @@
 package model;
 
 public class Movie {
+    private int id;
     private String title;
-    private String description;
-    private int duration; // in minutes
-    private String genre;
+    private String description; // Added
+    private int durationMinutes;
+    private String genre;       // Added
 
-    public Movie(String title, String description, int duration, String genre) {
+    // Constructor for creating new movies (without ID)
+    public Movie(String title, String description, int durationMinutes, String genre) {
         this.title = title;
         this.description = description;
-        this.duration = duration;
+        this.durationMinutes = durationMinutes;
         this.genre = genre;
     }
 
-    public String getTitle() { return title; }
-    public String getDescription() { return description; }
-    public int getDuration() { return duration; }
-    public String getGenre() { return genre; }
+    // Constructor for retrieving movies from the database (with ID)
+    public Movie(int id, String title, String description, int durationMinutes, String genre) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.durationMinutes = durationMinutes;
+        this.genre = genre;
+    }
 
-    public void setTitle(String title) { this.title = title; }
-    public void setDescription(String description) { this.description = description; }
-    public void setDuration(int duration) { this.duration = duration; }
-    public void setGenre(String genre) { this.genre = genre; }
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDurationMinutes(int durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 
     @Override
     public String toString() {
-        return title + " (" + genre + ") - " + duration + " mins";
+        return "Movie ID: " + id +
+                ", Title: '" + title + '\'' +
+                ", Description: '" + description + '\'' +
+                ", Duration: " + durationMinutes + " mins" +
+                ", Genre: '" + genre + '\'';
+        //git test comment
     }
 }
